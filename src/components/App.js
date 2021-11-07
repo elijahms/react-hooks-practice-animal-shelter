03d1493fc5a7715a51d5b0eb707879cabab76309
Elijah Silverman <elijahmsilverman@gmail.com>
2021-11-07 17:50:54 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,22 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onChangeType(event) {
+      setFilters({ type: event.target.value});
+  }
+
+  function onFindPetsClick() {
+    fetch(`http://localhost:3001/pets${filters.type !== "all" ? "?type=" + filters.type : ''}`)
+    .then(r => r.json())
+    .then(data => setPets(data));
+  }
+
+  console.log(pets);
+
+  function onAdoptPet(id) {
+    console.log("yo");
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +31,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,21 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({pets, onAdoptPet}) {
+
+  return <div className="ui cards">{pets.map((pet) => {
+    return <Pet 
+    key={pet.id}
+    type={pet.type}
+    name={pet.name}
+    gender={pet.gender}
+    age={pet.age}
+    weight={pet.weight}
+    isAdopted={pet.isAdopted}
+    id={pet.id}
+    />
+  })}</div>;
 }
 
 export default PetBrowser;
+
